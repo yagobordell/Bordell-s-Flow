695d11d3f36b31864403eee4645dadf94c56e909
Yago Bordell <yagobordell@gmail.com>
2026-08-26 19:30:15 +0200

    Export speech providers

--- src/ai_video_factory/providers/__init__.py
+++ src/ai_video_factory/providers/__init__.py
@@ -4,11 +4,16 @@ from .base import StructuredTextProvider
 from .images import GeneratedImage, ImageProvider
 from .openai import OpenAIProvider
 from .openai_images import OpenAIImageProvider
+from .openai_speech import OpenAISpeechProvider
+from .speech import GeneratedSpeech, SpeechProvider
 
 __all__ = [
     "GeneratedImage",
+    "GeneratedSpeech",
     "ImageProvider",
     "OpenAIImageProvider",
     "OpenAIProvider",
+    "OpenAISpeechProvider",
+    "SpeechProvider",
     "StructuredTextProvider",
 ]
