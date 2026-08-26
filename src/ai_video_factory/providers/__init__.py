a4980b6adfa85642ac1b346cb4c74fe73990e818
Yago Bordell <yagobordell@gmail.com>
2026-08-26 12:56:07 +0200

    Export image providers

--- src/ai_video_factory/providers/__init__.py
+++ src/ai_video_factory/providers/__init__.py
@@ -1,6 +1,14 @@
 """External model and API providers."""
 
 from .base import StructuredTextProvider
+from .images import GeneratedImage, ImageProvider
 from .openai import OpenAIProvider
+from .openai_images import OpenAIImageProvider
 
-__all__ = ["OpenAIProvider", "StructuredTextProvider"]
+__all__ = [
+    "GeneratedImage",
+    "ImageProvider",
+    "OpenAIImageProvider",
+    "OpenAIProvider",
+    "StructuredTextProvider",
+]
