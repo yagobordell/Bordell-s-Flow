3f9d6aa87792f1b78bdc16f67c563ffc1f1f2374
Yago Bordell <yagobordell@gmail.com>
2026-08-26 20:03:37 +0200

    Export beat timing bot

--- src/ai_video_factory/bots/__init__.py
+++ src/ai_video_factory/bots/__init__.py
@@ -4,6 +4,7 @@ Production bots use narrow system prompts and Structured Outputs. Autonomous age
 reserved for later verification and selective-regeneration stages.
 """
 
+from .beat_timing import BeatTimingBot
 from .beats import BeatExtractorBot
 from .continuity import ContinuityBot
 from .narrative_blocks import NarrativeBlockBot
@@ -13,6 +14,7 @@ from .visual_references import VisualReferenceBot
 
 __all__ = [
     "BeatExtractorBot",
+    "BeatTimingBot",
     "ContinuityBot",
     "NarrativeBlockBot",
     "ScenePlannerBot",
