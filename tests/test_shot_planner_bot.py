6da6a7e0297a7d6ff0208d4945351742cc518f0e
Yago Bordell <yagobordell@gmail.com>
2026-08-26 12:38:04 +0200

    style: sort shot planner test imports

--- tests/test_shot_planner_bot.py
+++ tests/test_shot_planner_bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import Any
 
-from ai_video_factory.bots.shots import PlannedShot, ShotPlanOutput, ShotPlannerBot
+from ai_video_factory.bots.shots import PlannedShot, ShotPlannerBot, ShotPlanOutput
 from ai_video_factory.domain import Beat, ContinuityEntity, Scene
 from ai_video_factory.providers.base import StatefulStructuredResult
 
