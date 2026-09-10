bdaa25ee18f5360465266dd3f2c76b9c702b3038
Yago Bordell <yagobordell@gmail.com>
2026-09-10 15:23:34 +0200

    Export Phase 9.3 Remotion compositor helpers

--- src/ai_video_factory/compositor/__init__.py
+++ src/ai_video_factory/compositor/__init__.py
@@ -1,6 +1,16 @@
 from .captions import build_caption_cues
 from .media import MediaProbe, probe_video
 from .models import CaptionCue, CaptionWord, CompositionPlan, CompositionShot
+from .remotion import (
+    PresentationNormalization,
+    RemotionCaptionCue,
+    RemotionCaptionWord,
+    RemotionRenderProps,
+    RemotionShot,
+    normalize_caption_display_text,
+    prepare_remotion_props,
+    validate_remotion_visual,
+)
 from .timeline import FrameInterval, quantize_shot_timings, seconds_to_frame
 from .workflow import build_composition_plan
 
@@ -11,9 +21,17 @@ __all__ = [
     "CompositionShot",
     "FrameInterval",
     "MediaProbe",
+    "PresentationNormalization",
+    "RemotionCaptionCue",
+    "RemotionCaptionWord",
+    "RemotionRenderProps",
+    "RemotionShot",
     "build_caption_cues",
     "build_composition_plan",
+    "normalize_caption_display_text",
+    "prepare_remotion_props",
     "probe_video",
     "quantize_shot_timings",
     "seconds_to_frame",
+    "validate_remotion_visual",
 ]
