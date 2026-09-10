d9ef9b68619701afe80bc764008461526e2f8ebe
Yago Bordell <yagobordell@gmail.com>
2026-09-10 15:25:42 +0200

    Render Phase 9.3 frame-exact visual timeline

---
+++ remotion/src/Phase9Visual.tsx
@@ -0,0 +1,31 @@
+import {Video} from "@remotion/media";
+import {AbsoluteFill, Sequence, staticFile} from "remotion";
+
+import {CaptionTrack} from "./CaptionTrack";
+import type {RemotionRenderProps} from "./types";
+
+export const Phase9Visual = ({shots, captions}: RemotionRenderProps) => {
+  return (
+    <AbsoluteFill style={{backgroundColor: "black"}}>
+      {shots.map((shot) => (
+        <Sequence
+          key={shot.shot_id}
+          from={shot.start_frame}
+          durationInFrames={shot.duration_frames}
+          name={`Shot ${shot.shot_id}`}
+        >
+          <Video
+            src={staticFile(shot.src.replace(/^\//, ""))}
+            muted
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </Sequence>
+      ))}
+      <CaptionTrack captions={captions} />
+    </AbsoluteFill>
+  );
+};
