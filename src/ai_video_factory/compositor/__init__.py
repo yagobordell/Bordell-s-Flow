d9d2aa28a76bba752fc260991f82004966df4a2f
Yago Bordell <yagobordell@gmail.com>
2026-09-10 16:34:53 +0200

    Export Phase 9.5 final mux helpers

--- src/ai_video_factory/compositor/__init__.py
+++ src/ai_video_factory/compositor/__init__.py
@@ -1,4 +1,17 @@
 from .captions import build_caption_cues
+from .final_mux import (
+    AudioStreamProbe,
+    FinalMuxInputs,
+    FinalMuxResult,
+    WavProbe,
+    build_final_mux_command,
+    mux_final_video,
+    parse_audio_stream_payload,
+    probe_audio_stream,
+    probe_wav,
+    validate_final_mux_inputs,
+    validate_final_mux_output,
+)
 from .media import MediaProbe, probe_video
 from .models import CaptionCue, CaptionWord, CompositionPlan, CompositionShot
 from .remotion import (
@@ -16,10 +29,13 @@ from .timeline import FrameInterval, quantize_shot_timings, seconds_to_frame
 from .workflow import build_composition_plan
 
 __all__ = [
+    "AudioStreamProbe",
     "CaptionCue",
     "CaptionWord",
     "CompositionPlan",
     "CompositionShot",
+    "FinalMuxInputs",
+    "FinalMuxResult",
     "FrameInterval",
     "MediaProbe",
     "PresentationNormalization",
@@ -28,12 +44,20 @@ __all__ = [
     "RemotionRenderProps",
     "RemotionShot",
     "RemotionVisualProfile",
+    "WavProbe",
     "build_caption_cues",
     "build_composition_plan",
+    "build_final_mux_command",
+    "mux_final_video",
     "normalize_caption_display_text",
+    "parse_audio_stream_payload",
     "prepare_remotion_props",
+    "probe_audio_stream",
     "probe_video",
+    "probe_wav",
     "quantize_shot_timings",
     "seconds_to_frame",
+    "validate_final_mux_inputs",
+    "validate_final_mux_output",
     "validate_remotion_visual",
 ]
