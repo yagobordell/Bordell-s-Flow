0afa38a8f76a97c24e1ba11b1fc7d53b81677e5e
Yago Bordell <yagobordell@gmail.com>
2026-09-10 14:58:48 +0200

    Export Phase 9.2 caption APIs

--- src/ai_video_factory/compositor/__init__.py
+++ src/ai_video_factory/compositor/__init__.py
@@ -1,14 +1,19 @@
+from .captions import build_caption_cues
 from .media import MediaProbe, probe_video
-from .models import CompositionPlan, CompositionShot
-from .timeline import FrameInterval, quantize_shot_timings
+from .models import CaptionCue, CaptionWord, CompositionPlan, CompositionShot
+from .timeline import FrameInterval, quantize_shot_timings, seconds_to_frame
 from .workflow import build_composition_plan
 
 __all__ = [
+    "CaptionCue",
+    "CaptionWord",
     "CompositionPlan",
     "CompositionShot",
     "FrameInterval",
     "MediaProbe",
+    "build_caption_cues",
     "build_composition_plan",
     "probe_video",
     "quantize_shot_timings",
+    "seconds_to_frame",
 ]
