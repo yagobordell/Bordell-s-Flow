57ff4cc12e63551cee147eff08aad6bc3f826bea
Yago Bordell <yagobordell@gmail.com>
2026-09-10 16:17:29 +0200

    Register Phase 9.4 motion composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,11 +1,12 @@
 import {Composition} from "remotion";
 
+import {Phase9Motion} from "./Phase9Motion";
 import {Phase9Visual} from "./Phase9Visual";
 import type {RemotionRenderProps} from "./types";
 import {validateRenderProps} from "./validation";
 
 const defaultProps: RemotionRenderProps = {
-  schema_version: "1",
+  schema_version: "2",
   width: 768,
   height: 1280,
   fps: 24,
@@ -13,27 +14,49 @@ const defaultProps: RemotionRenderProps = {
   shots: [],
   captions: [],
   presentation_normalizations: [],
+  visual_profile: {
+    transition_frames: 6,
+    transition_floor_opacity: 0.72,
+    transition_scale: 1.015,
+    caption_motion_frames: 4,
+    boundary_accent_frames: 5,
+    show_progress_bar: true,
+  },
+};
+
+const calculateMetadata = ({props}: {props: RemotionRenderProps}) => {
+  validateRenderProps(props);
+  return {
+    durationInFrames: props.total_frames,
+    fps: props.fps,
+    width: props.width,
+    height: props.height,
+  };
 };
 
 export const RemotionRoot = () => {
   return (
-    <Composition
-      id="Phase9Visual"
-      component={Phase9Visual}
-      durationInFrames={1}
-      fps={24}
-      width={768}
-      height={1280}
-      defaultProps={defaultProps}
-      calculateMetadata={({props}) => {
-        validateRenderProps(props);
-        return {
-          durationInFrames: props.total_frames,
-          fps: props.fps,
-          width: props.width,
-          height: props.height,
-        };
-      }}
-    />
+    <>
+      <Composition
+        id="Phase9Visual"
+        component={Phase9Visual}
+        durationInFrames={1}
+        fps={24}
+        width={768}
+        height={1280}
+        defaultProps={defaultProps}
+        calculateMetadata={calculateMetadata}
+      />
+      <Composition
+        id="Phase9Motion"
+        component={Phase9Motion}
+        durationInFrames={1}
+        fps={24}
+        width={768}
+        height={1280}
+        defaultProps={defaultProps}
+        calculateMetadata={calculateMetadata}
+      />
+    </>
   );
 };
