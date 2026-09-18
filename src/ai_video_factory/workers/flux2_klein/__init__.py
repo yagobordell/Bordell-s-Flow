7223ab144aacc6814c9933b628eb4e667779d561
Yago Bordell <yagobordell@gmail.com>
2026-09-18 13:39:20 +0200

    Add src/ai_video_factory/workers/flux2_klein/__init__.py

---
+++ src/ai_video_factory/workers/flux2_klein/__init__.py
@@ -0,0 +1,21 @@
+from .model import (
+    FLUX2_KLEIN_GENERATION_PROFILE,
+    FLUX2_KLEIN_KEYFRAME_TASK,
+    FLUX2_KLEIN_MODEL_ID,
+    FLUX2_KLEIN_MODEL_REVISION,
+    FLUX2_KLEIN_REFERENCE_TASK,
+    Flux2KleinImageTaskRunner,
+    flux2_klein_application_job_id,
+    flux2_klein_seed_for_job,
+)
+
+__all__ = [
+    "FLUX2_KLEIN_GENERATION_PROFILE",
+    "FLUX2_KLEIN_KEYFRAME_TASK",
+    "FLUX2_KLEIN_MODEL_ID",
+    "FLUX2_KLEIN_MODEL_REVISION",
+    "FLUX2_KLEIN_REFERENCE_TASK",
+    "Flux2KleinImageTaskRunner",
+    "flux2_klein_application_job_id",
+    "flux2_klein_seed_for_job",
+]
