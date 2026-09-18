1e645359d540e6e648aa083bca04cdde056d8a54
Yago Bordell <yagobordell@gmail.com>
2026-09-18 16:46:46 +0200

    feat(fish): add resident worker scaffolding

---
+++ src/ai_video_factory/workers/fish_speech/__init__.py
@@ -0,0 +1,27 @@
+from .model import (
+    FISH_SPEECH_CHUNKING_PROFILE,
+    FISH_SPEECH_GENERATION_PROFILE,
+    FISH_SPEECH_MODEL_ID,
+    FISH_SPEECH_MODEL_REVISION,
+    FISH_SPEECH_RUNTIME_COMMIT,
+    FISH_SPEECH_TASK,
+    FishSpeechBackend,
+    FishSpeechParameters,
+    FishSpeechTaskRunner,
+    fish_speech_application_job_id,
+)
+from .settings import FishSpeechWorkerSettings
+
+__all__ = [
+    "FISH_SPEECH_CHUNKING_PROFILE",
+    "FISH_SPEECH_GENERATION_PROFILE",
+    "FISH_SPEECH_MODEL_ID",
+    "FISH_SPEECH_MODEL_REVISION",
+    "FISH_SPEECH_RUNTIME_COMMIT",
+    "FISH_SPEECH_TASK",
+    "FishSpeechBackend",
+    "FishSpeechParameters",
+    "FishSpeechTaskRunner",
+    "FishSpeechWorkerSettings",
+    "fish_speech_application_job_id",
+]
