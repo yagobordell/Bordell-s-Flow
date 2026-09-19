916ad8810301f906149c4e75d31587ff37fc76e2
Yago Bordell <yagobordell@gmail.com>
2026-09-19 17:26:20 +0200

    Make broken Salad queue attachment recovery actionable

--- scripts/start_salad_optimized_prewarm.ps1
+++ scripts/start_salad_optimized_prewarm.ps1
@@ -1026,7 +1026,9 @@ while ((Get-Date) -lt $Deadline) {
             throw (
                 "$Service reached ready state but Salad did not attach container group " +
                 "'$GroupName' to queue '$QueueName' within " +
-                "${ReadyUnattachedTimeoutSeconds}s."
+                "${ReadyUnattachedTimeoutSeconds}s. Recreate the stopped group with " +
+                "manage_salad_validation.ps1 -Action Prepare -Service $Service " +
+                "-SkipBuild -Recreate before retrying."
             )
         }
     }
