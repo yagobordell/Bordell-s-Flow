3b08ab8f8933f0696cfccf6d10e22567aa2e0d07
Yago Bordell <yagobordell@gmail.com>
2026-09-19 13:15:59 +0200

    feat: add self-hosted Real-ESRGAN worker

---
+++ src/ai_video_factory/workers/realesrgan/__init__.py
@@ -0,0 +1,17 @@
+from .jobs import realesrgan_application_job_id
+from .model import (
+    REALESRGAN_GENERATION_PROFILE,
+    REALESRGAN_MODEL_NAME,
+    REALESRGAN_TASK,
+    RealESRGANParameters,
+    RealESRGANVideoTaskRunner,
+)
+
+__all__ = [
+    "REALESRGAN_GENERATION_PROFILE",
+    "REALESRGAN_MODEL_NAME",
+    "REALESRGAN_TASK",
+    "RealESRGANParameters",
+    "RealESRGANVideoTaskRunner",
+    "realesrgan_application_job_id",
+]
