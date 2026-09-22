9d551d88cfdafe90040c56c7d5109c1b5894c951
Yago Bordell <yagobordell@gmail.com>
2026-09-22 21:33:25 +0200

    Export LTX A2V worker contract

--- src/ai_video_factory/workers/ltx25/__init__.py
+++ src/ai_video_factory/workers/ltx25/__init__.py
@@ -1,6 +1,16 @@
 """Dedicated LTX-2.5 inference worker."""
 
-from .jobs import ltx_video_application_job_id
+from .a2v import (
+    LTX_A2V_DEFAULT_PROMPT,
+    LTX_A2V_GENERATION_PROFILE,
+    LTX_A2V_HARD_MAX_SECONDS,
+    LTX_A2V_RECOMMENDED_MAX_SECONDS,
+    LTX_A2V_TASK,
+    DirectLTX25A2VBackend,
+    LTXA2VParameters,
+    LTXA2VTaskRunner,
+)
+from .jobs import ltx_a2v_application_job_id, ltx_video_application_job_id
 from .model import (
     LTX_GENERATION_PROFILE,
     LTX_VIDEO_TASK,
@@ -13,6 +23,14 @@ from .model import (
 from .settings import LTX25WorkerSettings
 
 __all__ = [
+    "DirectLTX25A2VBackend",
+    "LTXA2VParameters",
+    "LTXA2VTaskRunner",
+    "LTX_A2V_DEFAULT_PROMPT",
+    "LTX_A2V_GENERATION_PROFILE",
+    "LTX_A2V_HARD_MAX_SECONDS",
+    "LTX_A2V_RECOMMENDED_MAX_SECONDS",
+    "LTX_A2V_TASK",
     "DirectLTX25Backend",
     "LTX25WorkerSettings",
     "LTX_GENERATION_PROFILE",
@@ -21,5 +39,6 @@ __all__ = [
     "LTXVideoParameters",
     "LTXVideoTaskRunner",
     "ltx_num_frames_for_duration",
+    "ltx_a2v_application_job_id",
     "ltx_video_application_job_id",
 ]
