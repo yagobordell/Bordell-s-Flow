9d98795aee4893a5570e3b8df7e1b98f9756347a
Yago Bordell <yagobordell@gmail.com>
2026-09-22 21:15:02 +0200

    feat(ltx25): export A2V worker contract

--- src/ai_video_factory/workers/ltx25/__init__.py
+++ src/ai_video_factory/workers/ltx25/__init__.py
@@ -1,11 +1,23 @@
 """Dedicated LTX-2.5 inference worker."""
 
-from .jobs import ltx_video_application_job_id
+from .a2v import (
+    LTX_A2V_DEFAULT_PROMPT,
+    LTX_A2V_GENERATION_PROFILE,
+    LTX_A2V_RECOMMENDED_MAX_SECONDS,
+    LTX_A2V_TASK,
+    DirectLTX25AudioToVideoBackend,
+    LTXA2VModelFiles,
+    LTXAudioToVideoParameters,
+    LTXAudioToVideoTaskRunner,
+    probe_audio,
+)
+from .jobs import ltx_a2v_application_job_id, ltx_video_application_job_id
 from .model import (
     LTX_GENERATION_PROFILE,
     LTX_VIDEO_TASK,
     DirectLTX25Backend,
     LTXModelFiles,
+    LTXPipelineModeController,
     LTXVideoParameters,
     LTXVideoTaskRunner,
     ltx_num_frames_for_duration,
@@ -13,13 +25,24 @@ from .model import (
 from .settings import LTX25WorkerSettings
 
 __all__ = [
+    "DirectLTX25AudioToVideoBackend",
     "DirectLTX25Backend",
     "LTX25WorkerSettings",
-    "LTX_GENERATION_PROFILE",
-    "LTX_VIDEO_TASK",
+    "LTXA2VModelFiles",
+    "LTXAudioToVideoParameters",
+    "LTXAudioToVideoTaskRunner",
     "LTXModelFiles",
+    "LTXPipelineModeController",
     "LTXVideoParameters",
     "LTXVideoTaskRunner",
+    "LTX_A2V_DEFAULT_PROMPT",
+    "LTX_A2V_GENERATION_PROFILE",
+    "LTX_A2V_RECOMMENDED_MAX_SECONDS",
+    "LTX_A2V_TASK",
+    "LTX_GENERATION_PROFILE",
+    "LTX_VIDEO_TASK",
+    "ltx_a2v_application_job_id",
     "ltx_num_frames_for_duration",
     "ltx_video_application_job_id",
+    "probe_audio",
 ]
